ccd57e656c07aa28f5a647e53d34a3856fa6589b
Ariel_Ibarra <arielibarra39@gmail.com>
2020-12-03 14:46:34 -0800

    refactor

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,35 +6,23 @@ class PeopleController < ApplicationController
       @people = current_user.people.all
     end 
 
-    def new
-      @person = current_user.people.new
-     # @person.gifts.build
-    end 
+#    def new
+#      @person = current_user.people.new
+#     # @person.gifts.build
+#    end 
 
-    def create 
-      
-      @person = current_user.gifts.build.build_person(person_params)
-      binding.pry
-      if @person.save
-        redirect_to people_path(@person)
-      else
-        redirect_to new_person_path
-      end 
-    end 
+#    def create 
+#      @person = current_user.gifts.build.build_person(person_params)
+#      binding.pry
+#      if @person.save
+#        redirect_to people_path(@person)
+#      else
+#        redirect_to new_person_path
+#      end 
+#    end 
 
     def show
       @person = Person.find_by(id: params[:id])
-    end 
-
-    def destroy
-      @person = Person.find_by(id: params[:id])
-      @person.destroy
-    end 
-
-    private
-
-    def person_params
-      params.require(:person).permit(:name, :relationship)
     end 
 
 end
