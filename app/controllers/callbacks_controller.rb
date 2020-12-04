9e3ae24ba5d03f11fe35e4f0eb50396ab188f037
Ariel_Ibarra <arielibarra39@gmail.com>
2020-12-03 18:26:23 -0800

    add flash messages

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,19 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     
     def github
-     # binding.pry
       @user = User.from_omniauth(request.env["omniauth.auth"])
-
-      sign_in_and_redirect @user 
+      if @user.persisted?
+        sign_in_and_redirect @user 
+        flash[:notice] = "Congratulations, you're signed up!"
+      else
+        flash[:error] = "There was a problem"
+        redirect_to new_user_registration_path
+      end 
     end
+
+    def failure
+      flash[:error] = "There was a problem"
+      redirect_to new_user_registration_path
+    end 
 
 end 
