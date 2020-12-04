d3dd1b0439c34953120d14b68d10aec56fbb7df9
Ariel_Ibarra <arielibarra39@gmail.com>
2020-12-03 20:37:29 -0800

    add scope

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,5 @@
 class Gift < ApplicationRecord
+    scope :most_expensive_gift, -> {where(cost: self.maximum(:cost))}
     validates :name, :cost, :notes, presence: true
     belongs_to :user
     belongs_to :person 
